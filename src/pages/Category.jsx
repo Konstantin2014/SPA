723e17ba77bb3700e04f95472d030c1cf8396ead
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-10-05 11:20:16 +0300

    страница с рецептом

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -8,7 +8,7 @@ import React from "react";
 function Category() {
   const { name } = useParams();
   const [meals, setMeals] = useState([]);
-  const { goBack } = useNavigate();
+  let navigate = useNavigate();
 
   useEffect(() => {
     getFilteredCategory(name).then((data) => setMeals(data.meals));
@@ -16,7 +16,7 @@ function Category() {
 
   return (
     <>
-      <button className="btn" onClick={goBack}>
+      <button className="btn orange accent-3" onClick={() => navigate(-1)}>
         Go Back
       </button>
       {!meals.length ? <Preloader /> : <MealList meals={meals} />}
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -6,7 +6,7 @@ import Preloader from "../components/Preloader";
 function Recipe() {
   const [recipe, setRecipe] = useState({});
   const { id } = useParams();
-  const { goBack } = useNavigate();
+  let navigate = useNavigate();
 
   useEffect(() => {
     getMealById(id).then((data) => {
@@ -15,17 +15,53 @@ function Recipe() {
   }, [id]);
   return (
     <>
-      <button className="btn" onClick={goBack}>
-        Go Back
-      </button>
       {!recipe.idMeal ? (
         <Preloader />
       ) : (
         <div className="recipe">
           <img src={recipe.strMealThumb} alt={recipe.strMeal} />
           <h1>{recipe.strMeal}</h1>
+          <h3>Category: {recipe.strCategory}</h3>
+          {recipe.strArea ? <h3>Area: {recipe.strArea}</h3> : null}
+          <p>{recipe.strInstructions}</p>
+          <table className="centred">
+            <thead>
+              <tr>
+                <th>Ingredient</th>
+                <th>Measure</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(recipe).map((key) => {
+                if (key.includes("Ingredient") && recipe[key]) {
+                  return (
+                    <tr key={key}>
+                      <td>{recipe[key]}</td>
+                      <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                    </tr>
+                  );
+                }
+                return null;
+              })}
+            </tbody>
+          </table>
+          {recipe.strYoutube ? (
+            <div className="row">
+              <h4 style={{ margin: "2rem 0 1.5rem " }}>Video Recipe</h4>
+              <iframe
+                title={id}
+                src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(
+                  -11
+                )}`}
+                allowfullscreen
+              />
+            </div>
+          ) : null}
         </div>
       )}
+      <button className="btn orange accent-3" onClick={() => navigate(-1)}>
+        Go Back
+      </button>
     </>
   );
 }
