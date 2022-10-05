ec86ce44d9e323809ad0b625b704f4c5b4f467bc
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-10-05 13:27:02 +0300

    Updates

---
+++ precache-manifest.23ebabbeac91861dddc3b8b4db4e89c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a888c92b08beac79722",
+    "url": "/SPA/static/css/main.0f356225.chunk.css"
+  },
+  {
+    "revision": "2a888c92b08beac79722",
+    "url": "/SPA/static/js/main.2a888c92.chunk.js"
+  },
+  {
+    "revision": "36d5c3b1560025246140",
+    "url": "/SPA/static/js/1.36d5c3b1.chunk.js"
+  },
+  {
+    "revision": "3b86541c40c399f4ce1f",
+    "url": "/SPA/static/js/runtime~main.3b86541c.js"
+  },
+  {
+    "revision": "19b5e825dcdd9f83404cd95bb49a5303",
+    "url": "/SPA/index.html"
+  }
+];
